dfc6024178db3a2a23faff9add7839ab916d537d
MOHD ZAID <144842290+mohd-zaid78@users.noreply.github.com>
2024-11-30 17:42:55 +0530

    Update MyProgram.java
    added lets rock message in rubba branch

--- MyProgram.java
+++ MyProgram.java
@@ -1,9 +1,7 @@
 class Program {
   public static void main (String [] args){
     System.out.println("mohd zaid");
-    System.out.println("make your personality good ");
-      System.out.println("for living happy life");
-	System.out.println("for  living a happy life");
-	  System.out.println("final ");
+    System.out.println("lets rock yrr");
+     
   }
 }
