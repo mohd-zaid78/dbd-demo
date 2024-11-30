ce119553a9067128e1e347fe00df16db2f27a4bc
MOHD ZAID <144842290+mohd-zaid78@users.noreply.github.com>
2024-11-30 17:44:48 +0530

    Update MyProgram.java
    added its star branch in star branch myprogramm file

--- MyProgram.java
+++ MyProgram.java
@@ -1,8 +1,6 @@
 class Program {
   public static void main (String [] args){
-    System.out.println("mohd zaid");
-    System.out.println("make your personality good ");
-      System.out.println("for living happy life");
-	System.out.println("for  living a happy life");
+    System.out.println("mohd zaid its star beanch ");
+   
   }
 }
