5274359c50299c5ed22b7db88aa87c9fc059a281
MOHD ZAID <144842290+mohd-zaid78@users.noreply.github.com>
2024-11-30 17:15:00 +0530

    Update MyProgram.java
    removing all messages accept on eand edit that one in main

--- MyProgram.java
+++ MyProgram.java
@@ -1,8 +1,6 @@
 class Program {
   public static void main (String [] args){
-    System.out.println("mohd zaid");
-    System.out.println("make your personality good ");
-    System.out.println("make your personality good ");
+    System.out.println("I am mohd zaid");
     
   }
 }
