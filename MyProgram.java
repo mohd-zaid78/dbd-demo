87de107de5518d0f844e972e1325b627c2ca15d3
mohd zaid <zaidgaur7@gmail.com>
2024-11-30 16:12:01 +0530

    added some messages

--- MyProgram.java
+++ MyProgram.java
@@ -3,5 +3,6 @@ class Program {
     System.out.println("mohd zaid");
     System.out.println("make your personality good ");
       System.out.println("for living happy life");
+	System.out.println("for  living a happy life");
   }
 }
