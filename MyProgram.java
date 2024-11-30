cd34f6ce8a81a76e7a602a1b72dcd78a9d416a13
MOHD ZAID <144842290+mohd-zaid78@users.noreply.github.com>
2024-11-30 15:59:58 +0530

    Update MyProgram.java
    added one more message

--- MyProgram.java
+++ MyProgram.java
@@ -2,5 +2,6 @@ class Program {
   public static void main (String [] args){
     System.out.println("mohd zaid");
     System.out.println("make your personality good ");
+      System.out.println("for living happy life");
   }
 }
